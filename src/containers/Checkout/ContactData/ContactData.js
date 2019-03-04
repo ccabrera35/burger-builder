8eb7b8053c598fc7a9f7bdd74982df1723e36205
Catherine Cabrera <45543137+ccabrera35@users.noreply.github.com>
2019-03-04 16:04:29 -0500

    add lazy loading

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,6 +8,7 @@ import { connect } from 'react-redux';
 import Input from '../../../components/UI/Input/Input';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
+import { updateObject, checkValidity } from '../../../shared/utility';
 
 
 
@@ -86,11 +87,11 @@ class ContactData extends Component {
                     elementType: 'select',
                     elementConfig: {
                         options: [
-                            {value: 'flying', displayValue: 'Flying'},
-                            {value: 'sailing', displayValue: 'Sailing'}
+                            {value: '30 min', displayValue: '30 min'},
+                            {value: '1 hr', displayValue: '1 hr'}
                         ]
                     },
-                    value: 'flying',
+                    value: '30 min',
                     validation: {},
                     valid: true
                 },
@@ -114,48 +115,16 @@ class ContactData extends Component {
         
     }
 
-    checkValidity(value, rules) {
-            let isValid = true;
-            if (!rules) {
-                return true;
-            }
-            
-            if (rules.required) {
-                isValid = value.trim() !== '' && isValid;
-            }
-        
-            if (rules.minLength) {
-                isValid = value.length >= rules.minLength && isValid
-            }
-        
-            if (rules.maxLength) {
-                isValid = value.length <= rules.maxLength && isValid
-            }
-        
-            if (rules.isEmail) {
-                const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-                isValid = pattern.test(value) && isValid
-            }
-        
-            if (rules.isNumeric) {
-                const pattern = /^\d+$/;
-                isValid = pattern.test(value) && isValid
-            }
-        
-            return isValid;
-        }
-
     inputChangedHandler = (event, inputIdentifier) => {
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        };
-        const updatedFormElement = {
-            ...updatedOrderForm[inputIdentifier]
-        };
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
-        updatedFormElement.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+           value: event.target.value,
+           valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+           touched: true,
+        });
+
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier]: updatedFormElement
+        });
 
         let formIsValid = true;
         for(let inputIdentifier in updatedOrderForm) {
